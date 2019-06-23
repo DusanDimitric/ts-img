2d8a5c253fd4a35afa7c0adb70d301c99543c8f2
Dusan Dimitric <dusan@mitipi.com>
2019-06-23 15:47:48 +0200

    Display images on the Front-End

--- src/web/static/js/app.js
+++ src/web/static/js/app.js
@@ -1,3 +1,35 @@
 (function() {
   'use strict'
+
+  class ImageThumbnailComponent {
+    static parentComponent = document.getElementById('image-list')
+      
+    constructor(imageData) {
+      this.imageData = imageData
+      this.element = this.render()
+    }
+
+    render() {
+      const imageLink = document.createElement('a')
+      imageLink.setAttribute('href', this.imageData.links.html)
+
+      const imageFigure = document.createElement('figure')
+      const imageFigcaption = document.createElement('figcaption')
+
+      imageFigcaption.textContent = this.imageData.alt_description
+
+      const imageElement = document.createElement('img')
+      imageElement.setAttribute('src', this.imageData.urls.thumb)
+      imageElement.setAttribute('alt', this.imageData.alt_description)
+
+      imageFigure.appendChild(imageElement)
+      imageFigure.appendChild(imageFigcaption)
+      imageLink.appendChild(imageFigure)
+      ImageThumbnailComponent.parentComponent.appendChild(imageLink)
+    }
+  }
+
+  const imageComponents = fetch('/images')
+    .then(images => images.json())
+    .then(images => images.map(img => new ImageThumbnailComponent(img)))
 })()
